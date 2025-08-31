930129105dd1d0f0d66b91dcfc97dada119d32c8
Miguel Angel Agustin Zuñiga Romo <miguel.zuniga0668@alumnos.udg.mx>
2025-08-30 23:09:36 -0600

    Add initial Flask app and dependencies
    Introduces a basic Flask application with a home route, a requirements file specifying Flask and Gunicorn, and includes tenis-app.zip. Sets up the project for local development and deployment.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+app = Flask(__name__) 
+
+@app.route("/") 
+def home(): 
+    return """ 
+    <!DOCTYPE html> 
+    <html> 
+    <head> 
+        <title>Tenis App</title> 
+        
+    </head> 
+    </html> 
+    """ 
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5500, debug=True) 
